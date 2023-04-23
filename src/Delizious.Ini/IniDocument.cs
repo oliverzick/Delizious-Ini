995f87ef1279b6d180459c8f0de4f7f8effedeab
Oliver Zick <oliverzick@users.noreply.github.com>
2023-04-23 17:04:05 +0200

    Document INI document

--- src/Delizious.Ini/IniDocument.cs
+++ src/Delizious.Ini/IniDocument.cs
@@ -8,6 +8,9 @@
     using System.IO;
     using System.Linq;
 
+    /// <summary>
+    /// Represents an INI document.
+    /// </summary>
     public sealed class IniDocument
     {
         private readonly IniData iniData;
@@ -17,6 +20,22 @@
             this.iniData = iniData;
         }
 
+        /// <summary>
+        /// Loads an INI document by reading the INI contents from the given <paramref name="textReader"/>. 
+        /// The <paramref name="textReader"/> is only used to read the INI contents from and not kept in the returned <see cref="IniDocument"/> instance.
+        /// </summary>
+        /// <param name="textReader">
+        /// The <see cref="TextReader"/> to read the INI contents from.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="IniDocument"/> instance that contains the INI contents read from the given <paramref name="textReader"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="textReader"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="IniException">
+        /// The INI document could not be loaded from the given <paramref name="textReader"/>.
+        /// </exception>
         public static IniDocument LoadFrom(TextReader textReader)
         {
             if (textReader is null)
@@ -50,6 +69,12 @@
             }
         }
 
+        /// <summary>
+        /// Provides the names of all sections contained in the current <see cref="IniDocument"/>.
+        /// </summary>
+        /// <returns>
+        /// The names of all sections contained in the current <see cref="IniDocument"/>
+        /// </returns>
         public IEnumerable<SectionName> SectionNames()
             => this.iniData.Sections.Select(section => SectionName.Create(section.SectionName));
     }
