e01ef239d0435664005634d45d2cf2eebc6a7b57
Oliver Zick <oliverzick@users.noreply.github.com>
2024-02-10 17:29:19 +0100

    Introduce value type that represents a property key

---
+++ src/Delizious.Ini.Test/PropertyKeySpec.cs
@@ -0,0 +1,111 @@
+﻿namespace Delizious.Ini.Test
+{
+    using System;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public sealed class PropertyKeySpec
+    {
+        private const string Key = "Key";
+
+        [TestMethod]
+        public void Throws_exception_on_creation_when_given_property_key_is_null()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => PropertyKey.Create(null));
+        }
+
+        [TestMethod]
+        public void Represents_encapsulated_property_key()
+        {
+            const string expected = Key;
+
+            PropertyKey target = expected;
+
+            var actual = target.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Provides_hash_code_of_encapsulated_property_key()
+        {
+            var expected = Key.GetHashCode();
+
+            PropertyKey target = Key;
+
+            var actual = target.GetHashCode();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_equality_operator(PropertyKey left, PropertyKey right, bool expected)
+        {
+            var actual = left == right;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_inequality_operator(PropertyKey left, PropertyKey right, bool inverse_expected)
+        {
+            var expected = !inverse_expected;
+
+            var actual = left != right;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Equality_operator_test_cases()
+        {
+            yield return new object[] { PropertyKeys.Null, PropertyKeys.Null, true };
+            yield return new object[] { PropertyKeys.A, PropertyKeys.Null, false };
+            yield return new object[] { PropertyKeys.Null, PropertyKeys.A, false };
+            yield return new object[] { PropertyKeys.A, PropertyKeys.B, false };
+            yield return new object[] { PropertyKeys.B, PropertyKeys.A, false };
+            yield return new object[] { PropertyKeys.A, PropertyKeys.A, true };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equals_method_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_equals_method(PropertyKey target, object other, bool expected)
+        {
+            var actual = target.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Equals_method_test_cases()
+        {
+            yield return new object[] { PropertyKeys.A, PropertyKeys.Null, false };
+            yield return new object[] { PropertyKeys.A, PropertyKeys.B, false };
+            yield return new object[] { PropertyKeys.A, PropertyKeys.A, true };
+            yield return new object[] { PropertyKeys.A, PropertyKeys.AnotherA, true };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(General_equals_method_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_method_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_general_equals_method(PropertyKey target, object other, bool expected)
+        {
+            var actual = target.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> General_equals_method_test_cases()
+        {
+            yield return new object[] { PropertyKeys.A, string.Empty, false };
+        }
+
+        private static class PropertyKeys
+        {
+            public static readonly PropertyKey Null = null!;
+            public static readonly PropertyKey A = "A";
+            public static readonly PropertyKey AnotherA = "A";
+            public static readonly PropertyKey B = "B";
+        }
+    }
+}
---
+++ src/Delizious.Ini/PropertyKey.cs
@@ -0,0 +1,73 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+
+    /// <summary>
+    /// Represents the key of a property in an <see cref="IniDocument"/>.
+    /// A property is also known as key-value pair.
+    /// </summary>
+    [Serializable]
+    public sealed class PropertyKey : IEquatable<PropertyKey>
+    {
+        private readonly string propertyKey;
+
+        private PropertyKey(string propertyKey)
+        {
+            this.propertyKey = propertyKey;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="PropertyKey"/> with the given <paramref name="propertyKey"/>.
+        /// </summary>
+        /// <param name="propertyKey">
+        /// The key of a property.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="PropertyKey"/> instance that represents the given <paramref name="propertyKey"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="propertyKey"/> is <c>null</c>.
+        /// </exception>
+        public static PropertyKey Create(string propertyKey)
+        {
+            if (propertyKey is null)
+            {
+                throw new ArgumentNullException(nameof(propertyKey));
+            }
+
+            return new PropertyKey(propertyKey);
+        }
+
+        public static implicit operator PropertyKey(string propertyKey)
+            => Create(propertyKey);
+
+        public static bool operator ==(PropertyKey left, PropertyKey right)
+            => Equals(left, right);
+
+        public static bool operator !=(PropertyKey left, PropertyKey right)
+            => !(left == right);
+
+        /// <inheritdoc/>
+        public bool Equals(PropertyKey other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return this.propertyKey == other.propertyKey;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as PropertyKey);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.propertyKey.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString()
+            => this.propertyKey;
+    }
+}
