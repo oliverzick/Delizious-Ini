ae70df79995809644009bcbf7a1383701a2ab14d
Oliver Zick <oliverzick@users.noreply.github.com>
2024-04-20 22:17:17 +0200

    Introduce serialization exception

---
+++ src/Delizious.Ini/SerializationException.cs
@@ -0,0 +1,27 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The exception that is thrown when the serialization of an <see cref="IniDocument"/> failed.
+    /// </summary>
+    [Serializable]
+    public sealed class SerializationException : IniException
+    {
+        private SerializationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        internal static SerializationException ForSerialization(Exception innerException)
+            => new SerializationException(ExceptionMessages.CouldNotSerializeIniDocument, innerException);
+
+        [ExcludeFromCodeCoverage]
+        private SerializationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
