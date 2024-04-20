87d85f3fdad1ead5cd71c34ffd513a2a4a29cb03
Oliver Zick <oliverzick@users.noreply.github.com>
2024-04-20 22:23:40 +0200

    Enable INI document to serialize to text writer

---
+++ src/Delizious.Ini.Test/SerializationExceptionAssertion.cs
@@ -0,0 +1,13 @@
+﻿namespace Delizious.Ini.Test
+{
+    using System;
+
+    internal sealed record SerializationExceptionAssertion(Type InnerExceptionType)
+    {
+        public static SerializationExceptionAssertion Create<T>() where T : Exception
+            => new(typeof(T));
+
+        public static implicit operator SerializationExceptionAssertion(SerializationException exception)
+            => new(exception.InnerException!.GetType());
+    }
+}
