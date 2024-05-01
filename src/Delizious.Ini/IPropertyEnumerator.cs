d1a35706166e1c4fd49b986ff1572b5ef2177802
Oliver Zick <oliverzick@users.noreply.github.com>
2024-05-01 20:31:56 +0200

    Segregate method to enumerate properties from INI document interface into dedicated property enumerator interface

---
+++ src/Delizious.Ini/IPropertyEnumerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Delizious.Ini
+{
+    using System.Collections.Generic;
+
+    internal interface IPropertyEnumerator
+    {
+        IEnumerable<PropertyKey> EnumerateProperties(SectionName sectionName);
+    }
+}
