3cef8ed0098a47ec2b36097332c1cde8777fccc5
Oliver Zick <oliverzick@users.noreply.github.com>
2024-05-05 14:40:37 +0200

    Introduce property read mode

---
+++ src/Delizious.Ini.Test/PropertyReadModeSpec.cs
@@ -0,0 +1,123 @@
+﻿namespace Delizious.Ini.Test
+{
+    using System.Collections.Generic;
+
+    [TestClass]
+    public sealed class PropertyReadModeSpec
+    {
+        private static PropertyReadMode Fail => PropertyReadMode.Fail();
+        private static PropertyReadMode Fallback => PropertyReadMode.Fallback();
+        private static PropertyReadMode FallbackCustom => PropertyReadMode.Fallback("Fallback");
+        private static PropertyReadMode FallbackCustomDefault => PropertyReadMode.Fallback(string.Empty);
+
+        [DataTestMethod]
+        [DynamicData(nameof(Provides_string_representation_test_cases), DynamicDataSourceType.Method)]
+        public void Provides_string_representation(PropertyReadMode target, string expected)
+        {
+            var actual = target.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Provides_string_representation_test_cases()
+        {
+            yield return new object[] { Fail, "Fail" };
+            yield return new object[] { Fallback, "Fallback" };
+            yield return new object[] { FallbackCustom, "Fallback" };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_equality_operator(PropertyReadMode left, PropertyReadMode right, bool expected)
+        {
+            var actual = left == right;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_inequality_operator(PropertyReadMode left, PropertyReadMode right, bool inverse_expected)
+        {
+            var expected = !inverse_expected;
+
+            var actual = left != right;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_equals_method(PropertyReadMode target, object other, bool expected)
+        {
+            var actual = target.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(General_equals_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_general_equals_method(PropertyReadMode target, object other, bool expected)
+        {
+            var actual = target.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+        public void Properly_implements_get_hash_code_method(PropertyReadMode target, PropertyReadMode other, bool expected)
+        {
+            var actual = target.GetHashCode() == other.GetHashCode();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Equality_operator_test_cases()
+        {
+            yield return new object[] { null!, null!, true };
+            yield return new object[] { null!, Fail, false };
+            yield return new object[] { null!, Fallback, false };
+            yield return new object[] { null!, FallbackCustom, false };
+        }
+
+        public static IEnumerable<object[]> Equals_null_test_cases()
+        {
+            yield return new object[] { Fail, null!, false };
+            yield return new object[] { Fallback, null!, false };
+            yield return new object[] { FallbackCustom, null!, false };
+        }
+
+        public static IEnumerable<object[]> Equals_test_cases()
+        {
+            yield return new object[] { Fail, Fail, true };
+            yield return new object[] { Fail, Fallback, false };
+            yield return new object[] { Fail, FallbackCustom, false };
+
+            yield return new object[] { Fallback, Fallback, true };
+            yield return new object[] { Fallback, FallbackCustom, false };
+            yield return new object[] { Fallback, Fail, false };
+
+            yield return new object[] { FallbackCustom, FallbackCustom, true };
+            yield return new object[] { FallbackCustom, Fallback, false };
+            yield return new object[] { FallbackCustom, Fail, false };
+
+            // Specifies that default fallback is the same as custom fallback with empty string
+            yield return new object[] { Fallback, FallbackCustomDefault, true };
+        }
+
+        public static IEnumerable<object[]> General_equals_test_cases()
+        {
+            yield return new object[] { Fail, string.Empty, false };
+            yield return new object[] { Fallback, string.Empty, false };
+            yield return new object[] { FallbackCustom, string.Empty, false };
+        }
+    }
+}
---
+++ src/Delizious.Ini/PropertyReadMode.cs
@@ -0,0 +1,133 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+
+    /// <summary>
+    /// Specifies how an <see cref="IniDocument"/> should read a property.
+    /// </summary>
+    public sealed class PropertyReadMode : IEquatable<PropertyReadMode>
+    {
+        private readonly IMode mode;
+
+        private PropertyReadMode(IMode mode)
+        {
+            this.mode = mode;
+        }
+
+        /// <summary>
+        /// Specifies that reading a property should fail by
+        /// throwing a <see cref="SectionNotFoundException"/> when the section does not exist,
+        /// or throwing a <see cref="PropertyNotFoundException"/> when the property does not exist.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="PropertyReadMode"/> instance that represents the fail mode.
+        /// </returns>
+        public static PropertyReadMode Fail()
+            => new PropertyReadMode(new FailMode());
+
+        /// <summary>
+        /// Specifies that reading a property should fall back to an empty fallback value
+        /// when the section or property does not exist.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="PropertyReadMode"/> instance that represents the fallback mode with an empty fallback value.
+        /// </returns>
+        public static PropertyReadMode Fallback()
+            => Fallback(string.Empty);
+
+        /// <summary>
+        /// Specifies that reading a property should fall back to the given <paramref name="fallbackPropertyValue"/>
+        /// when the section or property does not exist.
+        /// </summary>
+        /// <param name="fallbackPropertyValue">
+        /// The property value to fall back when the section or property does not exist.
+        /// </param>
+        /// <returns>
+        /// A <see cref="PropertyReadMode"/> instance that represents the fallback mode with the given <paramref name="fallbackPropertyValue"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="fallbackPropertyValue"/> is <c>null</c>.
+        /// </exception>
+        public static PropertyReadMode Fallback(PropertyValue fallbackPropertyValue)
+            => new PropertyReadMode(new FallbackMode(fallbackPropertyValue ?? throw new ArgumentNullException(nameof(fallbackPropertyValue))));
+
+        public static bool operator ==(PropertyReadMode left, PropertyReadMode right)
+            => Equals(left, right);
+
+        public static bool operator !=(PropertyReadMode left, PropertyReadMode right)
+            => !(left == right);
+
+        /// <inheritdoc/>
+        public bool Equals(PropertyReadMode other)
+            => !(other is null) && this.mode.Equals(other.mode);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as PropertyReadMode);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.mode.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString()
+            => this.mode.ToString();
+
+        internal T Transform<T>(IPropertyReadModeTransformation<T> transformation)
+            => this.mode.Transform(transformation);
+
+        private interface IMode
+        {
+            T Transform<T>(IPropertyReadModeTransformation<T> transformation);
+
+            bool Equals(IMode other);
+        }
+
+        private sealed class FailMode : IMode
+        {
+            public T Transform<T>(IPropertyReadModeTransformation<T> transformation)
+                => transformation.Fail();
+
+            public bool Equals(IMode other)
+                => other is FailMode;
+
+            public override int GetHashCode()
+                => nameof(FailMode).GetHashCode();
+
+            public override string ToString()
+                => "Fail";
+        }
+
+        private sealed class FallbackMode : IMode
+        {
+            private readonly PropertyValue fallbackPropertyValue;
+
+            public FallbackMode(PropertyValue fallbackPropertyValue)
+            {
+                this.fallbackPropertyValue = fallbackPropertyValue;
+            }
+
+            public T Transform<T>(IPropertyReadModeTransformation<T> transformation)
+                => transformation.Fallback(this.fallbackPropertyValue);
+
+            public bool Equals(IMode other)
+                => other is FallbackMode otherMode && this.Equals(otherMode);
+
+            private bool Equals(FallbackMode other)
+                => this.fallbackPropertyValue.Equals(other.fallbackPropertyValue);
+
+            public override int GetHashCode()
+                => this.fallbackPropertyValue.GetHashCode();
+
+            public override string ToString()
+                => $"Fallback";
+        }
+    }
+
+    internal interface IPropertyReadModeTransformation<out T>
+    {
+        T Fail();
+
+        T Fallback(PropertyValue fallbackPropertyValue);
+    }
+}
