08e0f690790b33b3a9cc7fc20e35ef111448aacd
Oliver Zick <oliverzick@users.noreply.github.com>
2024-06-30 14:14:04 +0200

    Improve performance by replacing exception-based apporach with strategy-based approach

--- src/Delizious.Ini/IniParserAdapter.cs
+++ src/Delizious.Ini/IniParserAdapter.cs
@@ -5,6 +5,7 @@
     using IniParser.Parser;
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Linq;
 
@@ -62,155 +63,21 @@
             => this.iniData.Sections.Select(section => SectionName.Create(section.SectionName));
 
         public IEnumerable<PropertyKey> EnumerateProperties(SectionName sectionName, PropertyEnumerationMode mode)
-            => mode.Transform(new PropertyEnumeratorSelector()).EnumerateProperties(this, sectionName);
-
-        private interface IPropertyEnumerator
-        {
-            IEnumerable<PropertyKey> EnumerateProperties(IniParserAdapter target, SectionName sectionName);
-        }
-
-        private sealed class PropertyEnumeratorSelector : IPropertyEnumerationModeTransformation<IPropertyEnumerator>
-        {
-            public IPropertyEnumerator Fail()
-                => new FailPropertyEnumerator();
-
-            private sealed class FailPropertyEnumerator : IPropertyEnumerator
-            {
-                public IEnumerable<PropertyKey> EnumerateProperties(IniParserAdapter target, SectionName sectionName)
-                    => target.SelectSection(sectionName).PropertyKeys();
-            }
-
-            public IPropertyEnumerator Fallback()
-                => new FallbackPropertyEnumerator();
-
-            private sealed class FallbackPropertyEnumerator : IPropertyEnumerator
-            {
-                public IEnumerable<PropertyKey> EnumerateProperties(IniParserAdapter target, SectionName sectionName)
-                {
-                    try
-                    {
-                        return target.SelectSection(sectionName).PropertyKeys();
-                    }
-                    catch (SectionNotFoundException)
-                    {
-                        return Enumerable.Empty<PropertyKey>();
-                    }
-                }
-            }
-        }
+            => this.SelectSection(sectionName, mode.Transform(new FallbackSectionProvider(sectionName)))
+                   .EnumerateProperties();
 
         public PropertyValue ReadProperty(SectionName sectionName, PropertyKey propertyKey, PropertyReadMode mode)
-            => mode.Transform(new PropertyReaderSelector()).ReadProperty(this, sectionName, propertyKey);
-
-        private interface IPropertyReader
-        {
-            PropertyValue ReadProperty(IniParserAdapter target, SectionName sectionName, PropertyKey propertyKey);
-        }
-
-        private sealed class PropertyReaderSelector : IPropertyReadModeTransformation<IPropertyReader>
-        {
-            public IPropertyReader Fail()
-                => new FailPropertyReader();
-
-            private sealed class FailPropertyReader : IPropertyReader
-            {
-                public PropertyValue ReadProperty(IniParserAdapter target, SectionName sectionName, PropertyKey propertyKey)
-                    => target.SelectSection(sectionName).ReadProperty(propertyKey);
-            }
-
-            public IPropertyReader Fallback(PropertyValue fallbackPropertyValue)
-                => new FallbackPropertyReader(fallbackPropertyValue);
-
-            private sealed class FallbackPropertyReader : IPropertyReader
-            {
-                private readonly PropertyValue fallbackPropertyValue;
-
-                public FallbackPropertyReader(PropertyValue fallbackPropertyValue)
-                {
-                    this.fallbackPropertyValue = fallbackPropertyValue;
-                }
-
-                public PropertyValue ReadProperty(IniParserAdapter target, SectionName sectionName, PropertyKey propertyKey)
-                {
-                    try
-                    {
-                        return target.SelectSection(sectionName).ReadProperty(propertyKey);
-                    }
-                    catch (SectionNotFoundException)
-                    {
-                        return this.fallbackPropertyValue;
-                    }
-                    catch (PropertyNotFoundException)
-                    {
-                        return this.fallbackPropertyValue;
-                    }
-                }
-            }
-        }
+            => this.SelectSection(sectionName, mode.Transform(new FallbackSectionProvider(sectionName)))
+                   .SelectProperty(propertyKey, mode.Transform(new FallbackPropertyProvider(propertyKey)))
+                   .ReadValue();
 
         public void WriteProperty(SectionName sectionName, PropertyKey propertyKey, PropertyValue propertyValue, PropertyWriteMode mode)
-            => mode.Transform(new PropertyWriterSelector()).WriteProperty(this, sectionName, propertyKey, propertyValue);
-
-        private interface IPropertyWriter
-        {
-            void WriteProperty(IniParserAdapter target, SectionName sectionName, PropertyKey propertyKey, PropertyValue propertyValue);
-        }
-
-        private sealed class PropertyWriterSelector : IPropertyWriteModeTransformation<IPropertyWriter>
-        {
-            public IPropertyWriter Create()
-                => new CreatePropertyWriter();
-
-            private sealed class CreatePropertyWriter : IPropertyWriter
-            {
-                public void WriteProperty(IniParserAdapter target, SectionName sectionName, PropertyKey propertyKey, PropertyValue propertyValue)
-                    => target.CreateSection(sectionName)
-                             .SelectSection(sectionName)
-                             .CreateProperty(propertyKey)
-                             .UpdateProperty(propertyKey, propertyValue);
-            }
-
-            public IPropertyWriter Update()
-                => new UpdatePropertyWriter();
-
-            private sealed class UpdatePropertyWriter : IPropertyWriter
-            {
-                public void WriteProperty(IniParserAdapter target, SectionName sectionName, PropertyKey propertyKey, PropertyValue propertyValue)
-                    => target.SelectSection(sectionName).UpdateProperty(propertyKey, propertyValue);
-            }
-        }
+            => mode.Transform(new WritePropertySelector(this, sectionName, propertyKey))
+                   .WriteValue(propertyValue);
 
         public void DeleteSection(SectionName sectionName, SectionDeletionMode mode)
-            => mode.Transform(new SectionDeleterSelector()).DeleteSection(this, sectionName);
-
-        private interface ISectionDeleter
-        {
-            void DeleteSection(IniParserAdapter target, SectionName sectionName);
-        }
-
-        private sealed class SectionDeleterSelector : ISectionDeletionModeTransformation<ISectionDeleter>
-        {
-            public ISectionDeleter Fail()
-                => new FailSectionDeleter();
-
-            private sealed class FailSectionDeleter : ISectionDeleter
-            {
-                // ToDo: Rework!
-                public void DeleteSection(IniParserAdapter target, SectionName sectionName)
-                {
-                    if (!target.iniData.Sections.RemoveSection(sectionName.ToString())) throw new SectionNotFoundException(sectionName);
-                }
-            }
-
-            public ISectionDeleter Ignore()
-                => new IgnoreSectionDeleter();
-
-            private sealed class IgnoreSectionDeleter : ISectionDeleter
-            {
-                public void DeleteSection(IniParserAdapter target, SectionName sectionName)
-                    => target.iniData.Sections.RemoveSection(sectionName.ToString());
-            }
-        }
+            => this.SelectSection(sectionName, mode.Transform(new FallbackSectionProvider(sectionName)))
+                   .Delete();
 
         private IniParserAdapter CreateSection(SectionName sectionName)
         {
@@ -219,68 +86,221 @@
             return this;
         }
 
-        private Section SelectSection(SectionName sectionName)
+        private ISection SelectSection(SectionName sectionName, ISection fallbackSection)
         {
-            // Indexer returns a null reference when section does not exist, so we need to throw appropriate exception here
-            var properties = this.iniData.Sections[sectionName.ToString()] ?? throw new SectionNotFoundException(sectionName);
+            var keyDataCollection = this.iniData.Sections[sectionName.ToString()];
 
-            return Section.Create(properties);
+            return keyDataCollection is null ? fallbackSection : new ExistentSection(this.iniData, sectionName, keyDataCollection);
         }
 
-        private sealed class Section
+        private interface ISection
         {
-            private readonly KeyDataCollection properties;
+            void Delete();
 
-            private Section(KeyDataCollection properties)
+            ISection CreateProperty(PropertyKey propertyKey);
+
+            IEnumerable<PropertyKey> EnumerateProperties();
+
+            IProperty SelectProperty(PropertyKey propertyKey, IProperty fallbackProperty);
+        }
+
+        private sealed class NullSection : ISection
+        {
+            public void Delete()
             {
-                this.properties = properties;
             }
 
-            public static Section Create(KeyDataCollection properties)
-                => new Section(properties);
+            [ExcludeFromCodeCoverage]
+            public ISection CreateProperty(PropertyKey propertyKey)
+                => this;
 
-            public IEnumerable<PropertyKey> PropertyKeys()
-                => this.properties.Select(property => PropertyKey.Create(property.KeyName));
+            public IEnumerable<PropertyKey> EnumerateProperties()
+                => Enumerable.Empty<PropertyKey>();
 
-            public Section CreateProperty(PropertyKey propertyKey)
+            public IProperty SelectProperty(PropertyKey propertyKey, IProperty fallbackProperty)
+                => fallbackProperty;
+        }
+
+        private sealed class NonexistentSection : ISection
+        {
+            private readonly SectionName sectionName;
+
+            public NonexistentSection(SectionName sectionName)
             {
-                this.properties.AddKey(propertyKey.ToString());
+                this.sectionName = sectionName;
+            }
+
+            public void Delete()
+                => throw new SectionNotFoundException(this.sectionName);
+
+            [ExcludeFromCodeCoverage]
+            public ISection CreateProperty(PropertyKey propertyKey)
+                => throw new SectionNotFoundException(this.sectionName);
+
+            public IEnumerable<PropertyKey> EnumerateProperties()
+                => throw new SectionNotFoundException(this.sectionName);
+
+            public IProperty SelectProperty(PropertyKey propertyKey, IProperty fallbackProperty)
+                => throw new SectionNotFoundException(this.sectionName);
+        }
+
+        private sealed class ExistentSection : ISection
+        {
+            private readonly IniData owner;
+
+            private readonly SectionName sectionName;
+
+            private readonly KeyDataCollection keyDataCollection;
+
+            public ExistentSection(IniData owner, SectionName sectionName, KeyDataCollection keyDataCollection)
+            {
+                this.owner = owner;
+                this.sectionName = sectionName;
+                this.keyDataCollection = keyDataCollection;
+            }
+
+            public void Delete()
+                => this.owner.Sections.RemoveSection(this.sectionName.ToString());
+
+            public ISection CreateProperty(PropertyKey propertyKey)
+            {
+                this.keyDataCollection.AddKey(propertyKey.ToString());
 
                 return this;
             }
 
-            public PropertyValue ReadProperty(PropertyKey propertyKey)
-                => this.SelectProperty(propertyKey).ReadValue();
+            public IEnumerable<PropertyKey> EnumerateProperties()
+                => this.keyDataCollection.Select(property => PropertyKey.Create(property.KeyName));
 
-            public void UpdateProperty(PropertyKey propertyKey, PropertyValue propertyValue)
-                => this.SelectProperty(propertyKey).UpdateValue(propertyValue);
-
-            private Property SelectProperty(PropertyKey propertyKey)
+            public IProperty SelectProperty(PropertyKey propertyKey, IProperty fallbackProperty)
             {
-                // GetKeyData returns a null reference when property does not exist, so we need to throw appropriate exception here
-                var keyData = this.properties.GetKeyData(propertyKey.ToString()) ?? throw new PropertyNotFoundException(propertyKey);
+                var keyData = this.keyDataCollection.GetKeyData(propertyKey.ToString());
 
-                return Property.Create(keyData);
+                return keyData is null ? fallbackProperty : new ExistentProperty(keyData);
+            }
+        }
+
+        private interface IProperty
+        {
+            PropertyValue ReadValue();
+
+            void WriteValue(PropertyValue value);
+        }
+
+        private sealed class NullProperty : IProperty
+        {
+            private readonly PropertyValue propertyValue;
+
+            public NullProperty(PropertyValue propertyValue)
+            {
+                this.propertyValue = propertyValue;
             }
 
-            private sealed class Property
+            public PropertyValue ReadValue()
+                => this.propertyValue;
+
+            [ExcludeFromCodeCoverage]
+            public void WriteValue(PropertyValue value)
             {
-                private readonly KeyData property;
-
-                private Property(KeyData property)
-                {
-                    this.property = property;
-                }
-
-                public static Property Create(KeyData keyData)
-                    => new Property(keyData);
-
-                public PropertyValue ReadValue()
-                    => this.property.Value;
-
-                public void UpdateValue(PropertyValue newPropertyValue)
-                    => this.property.Value = newPropertyValue.ToString();
             }
+        }
+
+        private sealed class NonexistentProperty : IProperty
+        {
+            private readonly PropertyKey propertyKey;
+
+            public NonexistentProperty(PropertyKey propertyKey)
+            {
+                this.propertyKey = propertyKey;
+            }
+
+            public PropertyValue ReadValue()
+                => throw new PropertyNotFoundException(this.propertyKey);
+
+            public void WriteValue(PropertyValue value)
+                => throw new PropertyNotFoundException(this.propertyKey);
+        }
+
+        private sealed class ExistentProperty : IProperty
+        {
+            private readonly KeyData keyData;
+
+            public ExistentProperty(KeyData keyData)
+            {
+                this.keyData = keyData;
+            }
+
+            public PropertyValue ReadValue()
+                => this.keyData.Value;
+
+            public void WriteValue(PropertyValue value)
+                => this.keyData.Value = value.ToString();
+        }
+
+        private sealed class FallbackSectionProvider : ISectionDeletionModeTransformation<ISection>, IPropertyEnumerationModeTransformation<ISection>, IPropertyReadModeTransformation<ISection>
+        {
+            private readonly SectionName sectionName;
+
+            public FallbackSectionProvider(SectionName sectionName)
+            {
+                this.sectionName = sectionName;
+            }
+
+            public ISection Fail()
+                => new NonexistentSection(this.sectionName);
+
+            public ISection Fallback()
+                => new NullSection();
+
+            public ISection Fallback(PropertyValue fallbackPropertyValue)
+                => new NullSection();
+
+            public ISection Ignore()
+                => new NullSection();
+        }
+
+        private sealed class FallbackPropertyProvider : IPropertyReadModeTransformation<IProperty>
+        {
+            private readonly PropertyKey propertyKey;
+
+            public FallbackPropertyProvider(PropertyKey propertyKey)
+            {
+                this.propertyKey = propertyKey;
+            }
+
+            public IProperty Fail()
+                => new NonexistentProperty(propertyKey);
+
+            public IProperty Fallback(PropertyValue fallbackPropertyValue)
+                => new NullProperty(fallbackPropertyValue);
+        }
+
+        private sealed class WritePropertySelector : IPropertyWriteModeTransformation<IProperty>
+        {
+            private readonly IniParserAdapter owner;
+
+            private readonly SectionName sectionName;
+
+            private readonly PropertyKey propertyKey;
+
+            public WritePropertySelector(IniParserAdapter owner, SectionName sectionName, PropertyKey propertyKey)
+            {
+                this.owner = owner;
+                this.sectionName = sectionName;
+                this.propertyKey = propertyKey;
+            }
+
+            public IProperty Create()
+                => this.owner
+                       .CreateSection(this.sectionName)
+                       .SelectSection(this.sectionName, new NonexistentSection(this.sectionName))
+                       .CreateProperty(this.propertyKey)
+                       .SelectProperty(this.propertyKey, new NonexistentProperty(this.propertyKey));
+
+            public IProperty Update()
+                => this.owner
+                       .SelectSection(this.sectionName, new NonexistentSection(this.sectionName))
+                       .SelectProperty(this.propertyKey, new NonexistentProperty(this.propertyKey));
         }
     }
 }
