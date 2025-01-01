8304a2cc9ef9c9ff1a9fc9a5666cd53ccec16fa1
Oliver Zick <oliverzick@users.noreply.github.com>
2025-01-01 22:15:42 +0100

    Document property assignment separator

--- src/QuickStart/Program.cs
+++ src/QuickStart/Program.cs
@@ -62,6 +62,7 @@ void Configure()
         IniDocumentConfiguration.Default
                                 .WithCaseSensitivity(CaseSensitivity.CaseInsensitive) // Treat section names and property keys as case-insensitive
                                 .WithInvalidLineBehavior(InvalidLineBehavior.Ignore) // Ignore when a line is invalid and cannot be parsed on loading
+                                .WithPropertyAssignmentSeparator(PropertyAssignmentSeparator.Default) // Use default property assignment separator which is equality sign '='
                                 .WithPropertyEnumerationMode(PropertyEnumerationMode.Fallback) // Fallback to empty collection of property keys when section does not exist
                                 .WithPropertyReadMode(PropertyReadMode.Fallback) // Fallback to empty string when property to read does not exist
                                 .WithPropertyWriteMode(PropertyWriteMode.Create) // Create a new property or update an existing property
@@ -74,6 +75,7 @@ void Configure()
         IniDocumentConfiguration.Default
                                 .WithCaseSensitivity(CaseSensitivity.CaseInsensitive) // Treat section names and property keys as case-insensitive
                                 .WithInvalidLineBehavior(InvalidLineBehavior.Fail) // Throw exception when a line is invalid and cannot be parsed on loading
+                                .WithPropertyAssignmentSeparator(PropertyAssignmentSeparator.Default) // Use default property assignment separator which is equality sign '='
                                 .WithPropertyEnumerationMode(PropertyEnumerationMode.Fail) // Throw exception when section to enumerate properties does not exist
                                 .WithPropertyReadMode(PropertyReadMode.Fail) // Throw exception when property to read to does not exist
                                 .WithPropertyWriteMode(PropertyWriteMode.Update) // Update existing property only but throw exception when property to write does not exist
