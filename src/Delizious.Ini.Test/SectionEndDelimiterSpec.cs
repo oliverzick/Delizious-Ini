86bd7ac420fc7b9590345389249c12d7fb3cc09c
Oliver Zick <oliverzick@users.noreply.github.com>
2025-01-23 22:17:22 +0100

    Introduce section end delimiter

---
+++ src/Delizious.Ini.Test/SectionEndDelimiterSpec.cs
@@ -0,0 +1,119 @@
+﻿namespace Delizious.Ini.Test;
+
+using System.Collections.Generic;
+
+[TestClass]
+public sealed class SectionEndDelimiterSpec
+{
+    private static SectionEndDelimiter Null => null!;
+    private static SectionEndDelimiter Default => SectionEndDelimiter.Default;
+    private static SectionEndDelimiter Bracket => ']';
+    private static SectionEndDelimiter Parentheses => ')';
+
+    [DataTestMethod]
+    [DynamicData(nameof(Provides_string_representation_test_cases), DynamicDataSourceType.Method)]
+    public void Provides_string_representation(SectionEndDelimiter target, string expected)
+    {
+        var actual = target.ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Provides_string_representation_test_cases()
+    {
+        yield return [Default, "]"];
+        yield return [Bracket, "]"];
+        yield return [Parentheses, ")"];
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_equality_operator(SectionEndDelimiter left, SectionEndDelimiter right, bool expected)
+    {
+        var actual = left == right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_inequality_operator(SectionEndDelimiter left, SectionEndDelimiter right, bool inverse_expected)
+    {
+        var expected = !inverse_expected;
+
+        var actual = left != right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases),      DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_equals_method(SectionEndDelimiter target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(General_equals_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),         DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),    DynamicDataSourceType.Method)]
+    public void Properly_implements_general_equals_method(SectionEndDelimiter target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_get_hash_code_method(SectionEndDelimiter target, SectionEndDelimiter other, bool expected)
+    {
+        var actual = target.GetHashCode() == other.GetHashCode();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Equality_operator_test_cases()
+    {
+        yield return [Null, Null, true];
+        yield return [Null, Default, false];
+        yield return [Null, Bracket, false];
+        yield return [Null, Parentheses, false];
+    }
+
+    public static IEnumerable<object[]> Equals_null_test_cases()
+    {
+        yield return [Default, Null, false];
+        yield return [Bracket, Null, false];
+        yield return [Parentheses, Null, false];
+    }
+
+    public static IEnumerable<object[]> Equals_test_cases()
+    {
+        yield return [Default, Default, true];
+        yield return [Default, Bracket, true];
+        yield return [Default, Parentheses, false];
+
+        yield return [Bracket, Default, true];
+        yield return [Bracket, Bracket, true];
+        yield return [Bracket, Parentheses, false];
+
+        yield return [Parentheses, Default, false];
+        yield return [Parentheses, Bracket, false];
+        yield return [Parentheses, Parentheses, true];
+    }
+
+    public static IEnumerable<object[]> General_equals_test_cases()
+    {
+        yield return [Default, new(), false];
+        yield return [Bracket, new(), false];
+        yield return [Parentheses, new(), false];
+    }
+}
---
+++ src/Delizious.Ini/SectionEndDelimiter.cs
@@ -0,0 +1,63 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+
+    /// <summary>
+    /// Represents the end delimiter of a section in an <see cref="IniDocument"/>.
+    /// </summary>
+    public sealed class SectionEndDelimiter : IEquatable<SectionEndDelimiter>
+    {
+        private readonly char delimiter;
+
+        private SectionEndDelimiter(char delimiter)
+        {
+            this.delimiter = delimiter;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SectionEndDelimiter"/> with the given delimiter.
+        /// </summary>
+        /// <param name="delimiter">
+        /// The delimiter.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="SectionEndDelimiter"/> instance that represents the given <paramref name="delimiter"/>.
+        /// </returns>
+        public static SectionEndDelimiter Create(char delimiter)
+            => new SectionEndDelimiter(delimiter);
+
+        public static implicit operator SectionEndDelimiter(char delimiter)
+            => Create(delimiter);
+
+        /// <summary>
+        /// The default end delimiter of a section which is the closing square bracket '<c>]</c>'.
+        /// </summary>
+        public static SectionEndDelimiter Default
+            => ']';
+
+        public static bool operator ==(SectionEndDelimiter left, SectionEndDelimiter right)
+            => Equals(left, right);
+
+        public static bool operator !=(SectionEndDelimiter left, SectionEndDelimiter right)
+            => !(left == right);
+
+        /// <inheritdoc/>
+        public bool Equals(SectionEndDelimiter other)
+            => !(other is null) && this.delimiter.Equals(other.delimiter);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as SectionEndDelimiter);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.delimiter.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString()
+            => this.delimiter.ToString();
+
+        internal char ToChar()
+            => this.delimiter;
+    }
+}
