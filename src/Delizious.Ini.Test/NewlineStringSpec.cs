6253a50b0deddcd7cf5be731eadb4eeab76b32eb
Oliver Zick <oliverzick@users.noreply.github.com>
2025-02-05 22:05:52 +0100

    Introduce newline string

---
+++ src/Delizious.Ini.Test/NewlineStringSpec.cs
@@ -0,0 +1,117 @@
+﻿namespace Delizious.Ini.Test;
+
+using System.Collections.Generic;
+
+[TestClass]
+public sealed class NewlineStringSpec
+{
+    private static NewlineString Null => null!;
+    private static NewlineString Environment => NewlineString.Environment;
+    private static NewlineString Unix => NewlineString.Unix;
+    private static NewlineString Windows => NewlineString.Windows;
+
+    [DataTestMethod]
+    [DynamicData(nameof(Provides_string_representation_test_cases), DynamicDataSourceType.Method)]
+    public void Provides_string_representation(NewlineString target, string expected)
+    {
+        var actual = target.ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Provides_string_representation_test_cases()
+    {
+        yield return [Environment, System.Environment.NewLine];
+        yield return [Unix, "\n"];
+        yield return [Windows, "\r\n"];
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_equality_operator(NewlineString left, NewlineString right, bool expected)
+    {
+        var actual = left == right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_inequality_operator(NewlineString left, NewlineString right, bool inverse_expected)
+    {
+        var expected = !inverse_expected;
+
+        var actual = left != right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases),      DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_equals_method(NewlineString target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(General_equals_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),         DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),    DynamicDataSourceType.Method)]
+    public void Properly_implements_general_equals_method(NewlineString target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_get_hash_code_method(NewlineString target, NewlineString other, bool expected)
+    {
+        var actual = target.GetHashCode() == other.GetHashCode();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Equality_operator_test_cases()
+    {
+        yield return [Null, Null, true];
+        yield return [Null, Environment, false];
+        yield return [Null, Unix, false];
+        yield return [Null, Windows, false];
+    }
+
+    public static IEnumerable<object[]> Equals_null_test_cases()
+    {
+        yield return [Environment, Null, false];
+        yield return [Unix, Null, false];
+        yield return [Windows, Null, false];
+    }
+
+    public static IEnumerable<object[]> Equals_test_cases()
+    {
+        // Environment newline string is not tested against other predefined newline strings
+        // to prevent coupling to specific underlying system
+        yield return [Environment, Environment, true];
+
+        yield return [Unix, Unix, true];
+        yield return [Unix, Windows, false];
+
+        yield return [Windows, Unix, false];
+        yield return [Windows, Windows, true];
+    }
+
+    public static IEnumerable<object[]> General_equals_test_cases()
+    {
+        yield return [Environment, new(), false];
+        yield return [Unix, new(), false];
+        yield return [Windows, new(), false];
+    }
+}
---
+++ src/Delizious.Ini/NewlineString.cs
@@ -0,0 +1,64 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+
+    /// <summary>
+    /// Represents a newline string that specifies a sequence of characters used to signify the end of a line.
+    /// </summary>
+    public sealed class NewlineString : IEquatable<NewlineString>
+    {
+        private readonly string newline;
+
+        private NewlineString(string newline)
+        {
+            this.newline = newline;
+        }
+
+        /// <summary>
+        /// The current newline string for this environment.
+        /// </summary>
+        public static NewlineString Environment
+            => new NewlineString(System.Environment.NewLine);
+
+        /// <summary>
+        /// The newline string <c>"\n"</c> for Unix systems.
+        /// </summary>
+        public static NewlineString Unix
+            => new NewlineString("\n");
+
+        /// <summary>
+        /// The newline string <c>"\r\n"</c> for Windows systems.
+        /// </summary>
+        public static NewlineString Windows
+            => new NewlineString("\r\n");
+
+        public static bool operator ==(NewlineString left, NewlineString right)
+            => Equals(left, right);
+
+        public static bool operator !=(NewlineString left, NewlineString right)
+            => !(left == right);
+
+        /// <inheritdoc/>
+        public bool Equals(NewlineString other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return this.newline == other.newline;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as NewlineString);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.newline.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString()
+            => this.newline;
+    }
+}
