80067d409b62ba5b6cc911bd342a156e6604f772
Oliver Zick <oliverzick@users.noreply.github.com>
2025-02-17 18:00:39 +0100

    Introduce section name regex

---
+++ src/Delizious.Ini.Test/SectionNameRegexSpec.cs
@@ -0,0 +1,127 @@
+﻿namespace Delizious.Ini.Test;
+
+using System;
+using System.Collections.Generic;
+
+[TestClass]
+public sealed class SectionNameRegexSpec
+{
+    private static SectionNameRegex Null => null!;
+    private static SectionNameRegex Default => SectionNameRegex.Default;
+    private static SectionNameRegex Custom => SectionNameRegex.Create(@"\w+");
+
+    [TestMethod]
+    public void Throws_argument_null_exception_on_creation_when_given_pattern_is_null()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => SectionNameRegex.Create(null));
+    }
+
+    [TestMethod]
+    public void Throws_argument_exception_on_creation_when_given_pattern_is_empty()
+    {
+        Assert.ThrowsException<ArgumentException>(() => SectionNameRegex.Create(string.Empty));
+    }
+
+    [TestMethod]
+    public void Throws_argument_exception_on_creation_when_given_pattern_is_invalid()
+    {
+        Assert.ThrowsException<ArgumentException>(() => SectionNameRegex.Create(@"\"));
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Provides_string_representation_test_cases), DynamicDataSourceType.Method)]
+    public void Provides_string_representation(SectionNameRegex target, string expected)
+    {
+        var actual = target.ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Provides_string_representation_test_cases()
+    {
+        yield return [Default, @"[\p{L}\p{M}\p{N}\p{P}\p{S}\p{Zs}]+"];
+        yield return [Custom, @"\w+"];
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_equality_operator(SectionNameRegex left, SectionNameRegex right, bool expected)
+    {
+        var actual = left == right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_inequality_operator(SectionNameRegex left, SectionNameRegex right, bool unexpected)
+    {
+        var expected = !unexpected;
+
+        var actual = left != right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases),      DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_equals_method(SectionNameRegex target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(General_equals_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),         DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),    DynamicDataSourceType.Method)]
+    public void Properly_implements_general_equals_method(SectionNameRegex target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_get_hash_code_method(SectionNameRegex target, SectionNameRegex other, bool expected)
+    {
+        var actual = target.GetHashCode() == other.GetHashCode();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Equality_operator_test_cases()
+    {
+        yield return [Null, Null, true];
+        yield return [Null, Default, false];
+        yield return [Null, Custom, false];
+    }
+
+    public static IEnumerable<object[]> Equals_null_test_cases()
+    {
+        yield return [Default, Null, false];
+        yield return [Custom, Null, false];
+    }
+
+    public static IEnumerable<object[]> Equals_test_cases()
+    {
+        yield return [Default, Default, true];
+        yield return [Default, Custom, false];
+
+        yield return [Custom, Default, false];
+        yield return [Custom, Custom, true];
+    }
+
+    public static IEnumerable<object[]> General_equals_test_cases()
+    {
+        yield return [Default, new(), false];
+        yield return [Custom, new(), false];
+    }
+}
---
+++ src/Delizious.Ini/SectionNameRegex.cs
@@ -0,0 +1,87 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Represents the regular expression for the name of a section in an <see cref="IniDocument"/>.
+    /// </summary>
+    public sealed class SectionNameRegex : IEquatable<SectionNameRegex>
+    {
+        private readonly string pattern;
+
+        private SectionNameRegex(string pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SectionNameRegex"/> with the given <paramref name="pattern"/>.
+        /// </summary>
+        /// <param name="pattern">
+        /// The regular expression pattern for the name of a section.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="SectionNameRegex"/> instance that represents the given <paramref name="pattern"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="pattern"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <para><paramref name="pattern"/> is <see cref="string.Empty"/>.</para>
+        /// <para>- or -</para>
+        /// <para><paramref name="pattern"/> is an invalid regular expression pattern.</para>
+        /// </exception>
+        public static SectionNameRegex Create(string pattern)
+        {
+            if (pattern is null)
+            {
+                throw new ArgumentNullException(nameof(pattern));
+            }
+
+            if (string.IsNullOrEmpty(pattern))
+            {
+                throw new ArgumentException(ExceptionMessages.PatternForSectionNameRegexMustNotBeEmpty, nameof(pattern));
+            }
+
+            try
+            {
+                _ = new Regex(pattern);
+            }
+            catch (ArgumentException exception)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidPatternForSectionNameRegex, nameof(pattern), exception);
+            }
+
+            return new SectionNameRegex(pattern);
+        }
+
+        /// <summary>
+        /// The default section regex which is '<c>[\p{L}\p{M}\p{N}\p{P}\p{S}\p{Zs}]+</c>'.
+        /// </summary>
+        public static SectionNameRegex Default
+            => Create(@"[\p{L}\p{M}\p{N}\p{P}\p{S}\p{Zs}]+");
+
+        public static bool operator ==(SectionNameRegex left, SectionNameRegex right)
+            => Equals(left, right);
+
+        public static bool operator !=(SectionNameRegex left, SectionNameRegex right)
+            => !(left == right);
+
+        /// <inheritdoc/>
+        public bool Equals(SectionNameRegex other)
+            => !(other is null) && this.pattern == other.pattern;
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as SectionNameRegex);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.pattern.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString()
+            => this.pattern;
+    }
+}
