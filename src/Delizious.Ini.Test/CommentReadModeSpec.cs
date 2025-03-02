b2a14a9c30bd4892055be9de52812538185314c7
Oliver Zick <oliverzick@users.noreply.github.com>
2025-03-02 21:41:09 +0100

    Introduce comment read mode that supports fail and fallback modes

---
+++ src/Delizious.Ini.Test/CommentReadModeSpec.cs
@@ -0,0 +1,129 @@
+﻿namespace Delizious.Ini.Test;
+
+using System;
+using System.Collections.Generic;
+
+[TestClass]
+public sealed class CommentReadModeSpec
+{
+    private static CommentReadMode Fail => CommentReadMode.Fail;
+    private static CommentReadMode Fallback => CommentReadMode.Fallback;
+    private static CommentReadMode FallbackCustom => CommentReadMode.CustomFallback("Fallback");
+    private static CommentReadMode FallbackCustomDefault => CommentReadMode.CustomFallback(string.Empty);
+
+    [TestMethod]
+    public void Throws_argument_null_exception_when_fallback_comment_is_null()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => CommentReadMode.CustomFallback(null));
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Provides_string_representation_test_cases), DynamicDataSourceType.Method)]
+    public void Provides_string_representation(CommentReadMode target, string expected)
+    {
+        var actual = target.ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Provides_string_representation_test_cases()
+    {
+        yield return [Fail, "Fail"];
+        yield return [Fallback, "Fallback"];
+        yield return [FallbackCustom, "Fallback"];
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_equality_operator(CommentReadMode left, CommentReadMode right, bool expected)
+    {
+        var actual = left == right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_inequality_operator(CommentReadMode left, CommentReadMode right, bool unexpected)
+    {
+        var expected = !unexpected;
+
+        var actual = left != right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases),      DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_equals_method(CommentReadMode target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(General_equals_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),         DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),    DynamicDataSourceType.Method)]
+    public void Properly_implements_general_equals_method(CommentReadMode target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_get_hash_code_method(CommentReadMode target, CommentReadMode other, bool expected)
+    {
+        var actual = target.GetHashCode() == other.GetHashCode();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Equality_operator_test_cases()
+    {
+        yield return [null!, null!, true];
+        yield return [null!, Fail, false];
+        yield return [null!, Fallback, false];
+        yield return [null!, FallbackCustom, false];
+    }
+
+    public static IEnumerable<object[]> Equals_null_test_cases()
+    {
+        yield return [Fail, null!, false];
+        yield return [Fallback, null!, false];
+        yield return [FallbackCustom, null!, false];
+    }
+
+    public static IEnumerable<object[]> Equals_test_cases()
+    {
+        yield return [Fail, Fail, true];
+        yield return [Fail, Fallback, false];
+        yield return [Fail, FallbackCustom, false];
+
+        yield return [Fallback, Fallback, true];
+        yield return [Fallback, FallbackCustom, false];
+        yield return [Fallback, Fail, false];
+
+        yield return [FallbackCustom, FallbackCustom, true];
+        yield return [FallbackCustom, Fallback, false];
+        yield return [FallbackCustom, Fail, false];
+
+        // Specifies that default fallback is the same as custom fallback with empty string
+        yield return [Fallback, FallbackCustomDefault, true];
+    }
+
+    public static IEnumerable<object[]> General_equals_test_cases()
+    {
+        yield return [Fail, new(), false];
+        yield return [Fallback, new(), false];
+        yield return [FallbackCustom, new(), false];
+    }
+}
---
+++ src/Delizious.Ini/CommentReadMode.cs
@@ -0,0 +1,136 @@
+﻿namespace Delizious.Ini
+{
+    using System;
+
+    /// <summary>
+    /// Specifies how an <see cref="IniDocument"/> should read a comment.
+    /// </summary>
+    public sealed class CommentReadMode : IEquatable<CommentReadMode>
+    {
+        private readonly IMode mode;
+
+        private CommentReadMode(IMode mode)
+        {
+            this.mode = mode;
+        }
+
+        /// <summary>
+        /// Specifies that reading a comment should fail by
+        /// throwing a <see cref="SectionNotFoundException"/> when the section does not exist,
+        /// or throwing a <see cref="PropertyNotFoundException"/> when the property does not exist.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="CommentReadMode"/> instance that represents the fail mode.
+        /// </returns>
+        public static CommentReadMode Fail
+            => new CommentReadMode(new FailMode());
+
+        /// <summary>
+        /// Specifies that reading a comment should fall back to an empty fallback value
+        /// when the section or property does not exist.
+        /// </summary>
+        /// <remarks>
+        /// This is a convenience property that calls <see cref="CustomFallback(Comment)"/> with an empty fallback value.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="CommentReadMode"/> instance that represents the fallback mode with an empty fallback value.
+        /// </returns>
+        public static CommentReadMode Fallback
+            => CustomFallback(string.Empty);
+
+        /// <summary>
+        /// Specifies that reading a comment should fall back to the given <paramref name="fallbackComment"/>
+        /// when the section or property does not exist.
+        /// </summary>
+        /// <param name="fallbackComment">
+        /// The comment to fall back when the section or property does not exist.
+        /// </param>
+        /// <returns>
+        /// A <see cref="CommentReadMode"/> instance that represents the fallback mode with the given <paramref name="fallbackComment"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="fallbackComment"/> is <c>null</c>.
+        /// </exception>
+        public static CommentReadMode CustomFallback(Comment fallbackComment)
+            => new CommentReadMode(new FallbackMode(fallbackComment ?? throw new ArgumentNullException(nameof(fallbackComment))));
+
+        public static bool operator ==(CommentReadMode left, CommentReadMode right)
+            => Equals(left, right);
+
+        public static bool operator !=(CommentReadMode left, CommentReadMode right)
+            => !(left == right);
+
+        /// <inheritdoc/>
+        public bool Equals(CommentReadMode other)
+            => !(other is null) && this.mode.Equals(other.mode);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as CommentReadMode);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.mode.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString()
+            => this.mode.ToString();
+
+        internal T Transform<T>(ICommentReadModeTransformation<T> transformation)
+            => this.mode.Transform(transformation);
+
+        private interface IMode
+        {
+            T Transform<T>(ICommentReadModeTransformation<T> transformation);
+
+            bool Equals(IMode other);
+        }
+
+        private sealed class FailMode : IMode
+        {
+            public T Transform<T>(ICommentReadModeTransformation<T> transformation)
+                => transformation.Fail();
+
+            public bool Equals(IMode other)
+                => other is FailMode;
+
+            public override int GetHashCode()
+                => nameof(FailMode).GetHashCode();
+
+            public override string ToString()
+                => "Fail";
+        }
+
+        private sealed class FallbackMode : IMode
+        {
+            private readonly Comment fallbackComment;
+
+            public FallbackMode(Comment fallbackComment)
+            {
+                this.fallbackComment = fallbackComment;
+            }
+
+            public T Transform<T>(ICommentReadModeTransformation<T> transformation)
+                => transformation.Fallback(this.fallbackComment);
+
+            public bool Equals(IMode other)
+                => other is FallbackMode otherMode && this.Equals(otherMode);
+
+            private bool Equals(FallbackMode other)
+                => this.fallbackComment.Equals(other.fallbackComment);
+
+            public override int GetHashCode()
+                => this.fallbackComment.GetHashCode();
+
+            public override string ToString()
+                => "Fallback";
+        }
+    }
+
+    internal interface ICommentReadModeTransformation<out T>
+    {
+        T Fail();
+
+        T Fallback(Comment fallbackComment);
+    }
+}
