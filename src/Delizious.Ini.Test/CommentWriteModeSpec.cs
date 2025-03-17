72c4fcd109591b67a6e09bd08b67b5a7ccf55c32
Oliver Zick <oliverzick@users.noreply.github.com>
2025-03-17 20:30:37 +0100

    Introduce comment write mode

---
+++ src/Delizious.Ini.Test/CommentWriteModeSpec.cs
@@ -0,0 +1,107 @@
+﻿namespace Delizious.Ini.Test;
+
+using System.Collections.Generic;
+
+[TestClass]
+public sealed class CommentWriteModeSpec
+{
+    private static CommentWriteMode Create => CommentWriteMode.Create;
+    private static CommentWriteMode Update => CommentWriteMode.Update;
+
+    [DataTestMethod]
+    [DynamicData(nameof(Provides_string_representation_test_cases), DynamicDataSourceType.Method)]
+    public void Provides_string_representation(CommentWriteMode target, string expected)
+    {
+        var actual = target.ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Provides_string_representation_test_cases()
+    {
+        yield return [Create, "Create"];
+        yield return [Update, "Update"];
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_equality_operator(CommentWriteMode left, CommentWriteMode right, bool expected)
+    {
+        var actual = left == right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equality_operator_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),            DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),       DynamicDataSourceType.Method)]
+    public void Properly_implements_inequality_operator(CommentWriteMode left, CommentWriteMode right, bool unexpected)
+    {
+        var expected = !unexpected;
+
+        var actual = left != right;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases),      DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_equals_method(CommentWriteMode target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(General_equals_test_cases), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_test_cases),         DynamicDataSourceType.Method)]
+    [DynamicData(nameof(Equals_null_test_cases),    DynamicDataSourceType.Method)]
+    public void Properly_implements_general_equals_method(CommentWriteMode target, object other, bool expected)
+    {
+        var actual = target.Equals(other);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Equals_test_cases), DynamicDataSourceType.Method)]
+    public void Properly_implements_get_hash_code_method(CommentWriteMode target, CommentWriteMode other, bool expected)
+    {
+        var actual = target.GetHashCode() == other.GetHashCode();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public static IEnumerable<object[]> Equality_operator_test_cases()
+    {
+        yield return [null!, null!, true];
+        yield return [null!, Create, false];
+        yield return [null!, Update, false];
+    }
+
+    public static IEnumerable<object[]> Equals_null_test_cases()
+    {
+        yield return [Create, null!, false];
+        yield return [Update, null!, false];
+    }
+
+    public static IEnumerable<object[]> Equals_test_cases()
+    {
+        yield return [Create, Create, true];
+        yield return [Create, Update, false];
+
+        yield return [Update, Update, true];
+        yield return [Update, Create, false];
+    }
+
+    public static IEnumerable<object[]> General_equals_test_cases()
+    {
+        yield return [Create, new(), false];
+        yield return [Update, new(), false];
+    }
+}
